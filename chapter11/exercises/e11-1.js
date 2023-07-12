f576b7548d54496d03f17aed301dc0e287df6b98
Joel Foust <joel.foust@yahoo.com>
2023-07-12 10:55:50 -0400

    Completed Chapter 11 Exercises.

--- chapter11/exercises/e11-1.js
+++ chapter11/exercises/e11-1.js
@@ -8,12 +8,27 @@ a) If passed a number, return the tripled value.
 b) If passed a string, return the string “ARRR!”
 c) Be sure to test your function before moving on the next part. */
 
-
-
-/* Use your fuction and the map method to change an array  as follows:
-a) Triple any the numbers.
-b) Replace any strings with “ARRR!”
-c) Print the new array to confirm your work.
-*/
-
-let arr = ['Elocution', 21, 'Clean teeth', 100];
+let returnArgument = function (arg) {
+    if (typeof arg === 'number') {
+        return arg * 3;
+    } else if (typeof arg === 'string') {
+        return "ARRR!";
+    } else {
+      return arg;
+    }
+  };
+  
+  console.log(returnArgument(10));
+  console.log(returnArgument(Number));
+  
+  /* Use your fuction and the map method to change an array  as follows:
+  a) Triple any the numbers.
+  b) Replace any strings with “ARRR!”
+  c) Print the new array to confirm your work.
+  */
+  
+  let arr = ['Elocution', 21, 'Clean teeth', 100];
+  
+  let newArray = arr.map(returnArgument);
+  console.log(arr);
+  console.log(newArray);
--- chapter11/exercises/e11-2.js
+++ chapter11/exercises/e11-2.js
@@ -1,28 +1,82 @@
 // 11.10.2. Raid a Shuttle
-
 function checkFuel(level) {
-    if (level > 100000){
-      return 'green';
-    } else if (level > 50000){
-      return 'yellow';
-    } else {
-      return 'red';
-    }
+  if (level > 100000){
+    return 'green';
+  } else if (level > 50000){
+    return 'yellow';
+  } else {
+    return 'red';
+  }
+}
+
+function holdStatus(arr){
+  if (arr.length < 7) {
+    return `Spaces available: ${7-arr.length}.`;
+  } else if (arr.length > 7){
+    return `Over capacity by ${arr.length-7} items.`;
+  } else {
+    return "Full";
+  }
+}
+
+let fuelLevel = 200000;
+
+let cargoHold = ['meal kits', 'space suits', 'first-aid kit', 'satellite', 'gold', 'water', 'AE-35 unit'];
+
+console.log("Fuel level: " + checkFuel(fuelLevel));
+console.log("Hold status: " + holdStatus(cargoHold));
+
+let nothingToSeeHere = function (n) {
+  
+  if (n > 100000) {
+    
+    n = n - 100001;
+    fuelLevel = fuelLevel - n;
+    
+  } else if (n <= 100000 && n >= 50000) {
+    
+    fuelLevel = 100001;
+    n = 0;
+    
+  } else if (n < 50000) {
+    
+    fuelLevel = 100001;
+    n = 0;
   }
   
-  function holdStatus(arr){
-    if (arr.length < 7) {
-      return `Spaces available: ${7-arr.length}.`;
-    } else if (arr.length > 7){
-      return `Over capacity by ${arr.length-7} items.`;
-    } else {
-      return "Full";
-    }
+  return n;
+};
+
+let iAmTheNight = function (arr) {
+  
+  let myItems = [];
+  
+  if (arr.length === 7 && arr.includes('gold')) {
+    
+      myItems.push(arr.splice(arr.indexOf('gold'),1,'oatmeal'));
+    
+  }   if  (arr.includes('AE-35 unit')) {
+    
+          myItems.push(arr.splice(arr.indexOf('AE-35 unit'),1,'paper clips'));
+    
   }
   
-  let fuelLevel = 200000;
-  let cargoHold = ['meal kits', 'space suits', 'first-aid kit', 'satellite', 'gold', 'water', 'AE-35 unit'];
+  return myItems;
   
-  console.log("Fuel level: " + checkFuel(fuelLevel));
-  console.log("Hold status: " + holdStatus(cargoHold));
+};
+
+let irs = function (fuelLevel, cargoHold) {
+  
+  let receipt = "";
+  let newItems = [];
+  
+  newItems = iAmTheNight(cargoHold);
+  
+  receipt = `\nRaided ${nothingToSeeHere(fuelLevel)} kg of fuel from tanks, and stole ${newItems[0]} and ${newItems[1]} from the cargo hold.`;
+  
+  return receipt;
+  
+};
+
+console.log(irs(fuelLevel, cargoHold));
   
