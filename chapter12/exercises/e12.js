200dbe397a6416600cb28674986673c7c2e7de37
Joel Foust <joel.foust@yahoo.com>
2023-07-12 23:28:32 -0400

    Worked on Chapter 12 Exercises

--- chapter12/exercises/e12.js
+++ chapter12/exercises/e12.js
@@ -38,9 +38,25 @@ let tardigrade = {
 // Create an array to hold the animal objects.
 let astronauts = [superChimpOne, salamander, chimpanzee, beagle, tardigrade];
 
-for (let i = 1; i <= astronauts.length; i++) {
-    astronauts[i-1].astronautID = i;
+let idArray = [1,2,3,4,5,6,7,8,9,10];
+
+for (let i = 0; i < astronauts.length; i++) {
+
+      if (!idArray.includes(astronauts[i].astronautID)) {
+        astronauts[i].astronautID = Math.floor(Math.random() * (10 - 1 + 1) + 1);
+      }
+    
+    
+
 }
+// Add a move method to generate random number of steps to move each animal. Values can be between and include 0 through 10.
+
+for (let i = 0; i < astronauts.length; i++) {
+    
+    astronauts[i].move = function() { return stepsToMove = Math.round(Math.random()*10);}
+}
+
+
 
 // Print out the relevant information about each animal.
 
@@ -48,3 +64,4 @@ for (let i = 0; i < astronauts.length; i++) {
     console.log(astronauts[i]);
 }
 // Start an animal race!
+
