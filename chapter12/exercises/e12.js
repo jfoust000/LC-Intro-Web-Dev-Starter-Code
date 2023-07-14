fcc1a252f85f2ac7f302b2f021328a7f76db1436
Joel Foust <joel.foust@yahoo.com>
2023-07-14 00:03:35 -0400

    Fixed Chapter 12 Exercise Fitness Test

--- chapter12/exercises/e12.js
+++ chapter12/exercises/e12.js
@@ -3,7 +3,9 @@ let superChimpOne = {
     species: "Chimpanzee",
     mass: 9,
     age: 6,
-    steps: 0
+    steps: 0,
+    stepsToMove: 0,
+    move:  function() { return this.stepsToMove = Math.round(Math.random()*10);}
 };
 
 let salamander = {
@@ -11,7 +13,9 @@ let salamander = {
     species: "Axolotl Salamander",
     mass: 0.1,
     age: 5,
-    steps: 0
+    steps: 0,
+    stepsToMove: 0,
+    move:  function() { return this.stepsToMove = Math.round(Math.random()*10);}
 };
 
 let chimpanzee = {
@@ -19,7 +23,9 @@ let chimpanzee = {
     species: "Chimpanzee",
     mass: 11,
     age: 6,
-    steps: 0
+    steps: 0,
+    stepsToMove: 0,
+    move:  function() { return this.stepsToMove = Math.round(Math.random()*10);}
 };
 
 let beagle = {
@@ -27,7 +33,9 @@ let beagle = {
     species: "Beagle",
     mass: 14,
     age: 5,
-    steps: 0
+    steps: 0,
+    stepsToMove: 0,
+    move:  function() { return this.stepsToMove = Math.round(Math.random()*10);}
 };
 
 let tardigrade = {
@@ -35,7 +43,9 @@ let tardigrade = {
     species: "Tardigrade",
     mass: 0.0000000001,
     age: 1,
-    steps: 0
+    steps: 0,
+    stepsToMove: 0,
+    move:  function() { return this.stepsToMove = Math.round(Math.random()*10);}
 };
 
 // After you have created the other object literals, add the astronautID property to each one.
@@ -54,59 +64,57 @@ for (let i = 0; i < astronauts.length; i++) {
     
 
 }
-// Add a move method to generate random number of steps to move each animal. Values can be between and include 0 through 10.
-
-for (let i = 0; i < astronauts.length; i++) {
-    
-    astronauts[i].move = function() { return stepsToMove = Math.round(Math.random()*10);}
-}
-
 
 function crewReports(animal) {
      
-    return `\n${animal.name} is a ${animal.species}. They are ${animal.age} years old and has a mass of ${animal.mass} kg. Their ID is ${animal.astronautID}.`;
+    return `\nAnimal Report:\n\n${animal.name} is a ${animal.species}. They are ${animal.age} years old and has a mass of ${animal.mass} kg. Their ID is ${animal.astronautID}.`;
 }
 
 function fitnessTest(crew) {
 
-    let n = 0;
-    let sortedFinalSteps = [];
-    let winner = '';
+    let numOfSteps = 0; //total steps the winner took to win
+    let winner = ''; //the winner's name
+    let breakout = false; //if the winner has reached 20 steps or more, break out of while loop.
 
-    while (crew[n].steps < 20) { 
-
+        //have all animals do a first move. should be no more than 10 steps each
         for (let i = 0; i < crew.length; i++) {
 
             crew[i].steps += crew[i].move();
+            
+        }
+        //now, have animals each make another move, but check after each one makes a move to see if they reached 20 steps or more. If they did, break out of the for loop and the while loop with breakout set to true.
+        while (numOfSteps < 20) {
+
+            if (breakout === false) {
+            
+                for (let i = 0; i < crew.length; i++) {
+                    
+                    crew[i].steps += crew[i].move();
+
+                    if (crew[i].steps >= 20) {
+                        winner = crew[i].name;
+                        numOfSteps = crew[i].steps;
+                        breakout = true;
+                        break;
+                    } 
+                 
+                }
+               
+             } else {
+
+                break;
+             }
+                 
+            numOfSteps++;
+                
+        }
         
-        }
+        return  `\nFitness Test:\n\n${winner} won the race with ${numOfSteps - 1} steps! Here are all of crew's steps:\n\n}`;
+        
     }
-    for (let i = 0; i < crew.length; i++) {
-        sortedFinalSteps.push(crew[i].steps);
-    }
-    sortedFinalSteps.sort(function(a, b) {
-            return a - b;
-        });
-    for (let i = 0; i < crew.length; i++) {
-        if (crew[i].steps === sortedFinalSteps[sortedFinalSteps.length - 1]) {   
-            winner = crew[i].name;
-        }
-    }
-    console.log(crew);
-    return  `${winner} is the winner of the race with ${sortedFinalSteps[sortedFinalSteps.length - 1]} steps!\n}`;
-}
     
-  
 
 
-
-
-// Print out the relevant information about each animal.
-
-// for (let i = 0; i < astronauts.length; i++) {
-//     console.log(astronauts[i]);
-// }
-// Start an animal race!
-
-//console.log(crewReports(astronauts[0]));
+console.log(crewReports(astronauts[0]));
 console.log(fitnessTest(astronauts));
+console.log(astronauts);
